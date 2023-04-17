379782326bbb71c2d6f5b9025e519ebdfc8a143e
ivu-u <123986386+IvTwo@users.noreply.github.com>
2023-04-17 11:21:02 -0400

    part 1 done

--- ExcelSorting/ExcelSorting/Program.cs
+++ ExcelSorting/ExcelSorting/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CsvHelper;
+using CsvHelper.Configuration;
 using System.Globalization;
 
 namespace ExcelSorting
@@ -21,38 +22,68 @@ namespace ExcelSorting
             List<PlayerRecords> recordList = new List<PlayerRecords>(records);
             #endregion
 
-            PrintRecords(recordList);
-
-            string[] columns = {"User_Name", "Weekly_Minutes_Played" };
-            Quicksort_CSV.Quicksort(recordList, 0, recordList.Count-1, columns);
-
-            PrintRecords(recordList);
+            UserInterface(ref recordList);
             
-            //// take user input. If there are X arguments then the progrma knows to sort by multiple column
-            //// otherwise the program defults to sorting by username
-            //string[] userInput = UserInput().Split(' ');
-            //string[] columns = userInput.Length > 0 ? userInput : new string[] { "User_Name" };
         }
 
-        public static void userInterface()
+        public static void UserInterface(ref List<PlayerRecords> recordList)
         {
             bool keepGoing = true;
+            int count = 0;
             while (keepGoing)
             {
                 Console.WriteLine();
-                Console.WriteLine("Welcome to the Card Guessing Game----!");
+                Console.WriteLine("Welcome to The Gamer Sort----!");
                 Console.WriteLine();
-                Console.WriteLine("\t1. Query\n" + "\t2. Guess\n" + "\t3. Quit\n");
+                Console.WriteLine("Press \"q\" to quit");
+                Console.WriteLine("Sort By...");
+                
 
-                switch (UserInput()) // handle user input
+                // take user input. If there are X arguments then the progrma knows to sort by multiple column
+                // otherwise the program defults to sorting by username
+                Console.WriteLine();
+                string userInput = UserInput();
+                string[] columnArray = { "", "" };
+                
+                switch (userInput.ToLower())   // handle user input
                 {
-                    case "1":
+                    case "name time":
+                        columnArray[0] = "User_Name";
+                        columnArray[1] = "Weekly_Minutes_Played";
                         break;
 
-                    case "2":
+                    case "name iap":
+                        columnArray[0] = "User_Name";
+                        columnArray[1] = "Weekly_IAP";
                         break;
 
-                    case "3":
+                    case "game time":
+                        columnArray[0] = "Game";
+                        columnArray[1] = "Weekly_Minutes_Played";
+                        break;
+
+                    case "game iap":
+                        columnArray[0] = "Game";
+                        columnArray[1] = "Weekly_IAP";
+                        break;
+
+                    case "name":
+                        columnArray[0] = "User_Name";
+                        break;
+
+                    case "game":
+                        columnArray[0] = "Game";
+                        break;
+
+                    case "time":
+                        columnArray[1] = "Weekly_Minutes_Played";
+                        break;
+
+                    case "iap":
+                        columnArray[0] = "Weekly_IAP";
+                        break;
+
+                    case "q":
                         keepGoing = false;
                         break;
 
@@ -60,6 +91,29 @@ namespace ExcelSorting
                         Console.WriteLine("---");
                         Console.WriteLine("Invalid Input!");
                         break;
+                }
+                string[] columns = userInput.Length > 0 ? columnArray : new string[] { "User_Name" };
+
+
+                Quicksort_CSV.Quicksort(recordList, 0, recordList.Count-1, columns);
+                PrintRecords(recordList);
+
+                // continue?
+                Console.WriteLine();
+                Console.WriteLine("Would you like to continue (y = yes | n = no)");
+                if (UserInput().Equals("n"))
+                {
+                    keepGoing = false;
+                }
+
+                // print results to a new csv file
+                count++;
+                string outputFile = "output" + count.ToString();
+                using (StreamWriter writer = new StreamWriter(outputFile + ".csv"))
+                using (CsvWriter csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csvWriter.Context.RegisterClassMap<PlayerRecordMap>();
+                    csvWriter.WriteRecords(recordList);
                 }
             }
         }
@@ -84,6 +138,17 @@ namespace ExcelSorting
                 Console.WriteLine(string.Format("  {0,-12} | {1,-6} | {2,-14} | {3,-10}", record.User_name, record.Game, record.Weekly_Minutes_Played, record.Weekly_IAP));
             }
             Console.WriteLine("-----------------------------------------------------");
+        }
+
+        public sealed class PlayerRecordMap : ClassMap<PlayerRecords>
+        {
+            public PlayerRecordMap()
+            {
+                Map(m => m.User_name).Name("User_name");
+                Map(m => m.Game).Name("Game");
+                Map(m => m.Weekly_Minutes_Played).Name("Weekly_Minutes_Played");
+                Map(m => m.Weekly_IAP).Name("Weekly_IAP");
+            }
         }
     }
 }
